139c78cea7954847b630a74d29326f83c1062123
Oleg Afanasev <oleg.afanasev@rt.ru>
2020-10-21 21:31:10 +0300

    Add init.jsx

--- src/components/UserContext.jsx
+++ src/components/UserContext.jsx
@@ -3,20 +3,22 @@ import faker from 'faker';
 import cookies from 'js-cookie';
 import isEmpty from 'lodash/isEmpty';
 
-// eslint-disable-next-line functional/no-let
-let nickname = cookies.get('nickname');
-
-if (isEmpty(nickname)) {
-  nickname = faker.name.findName();
-  cookies.set('nickname', nickname);
-}
-
 const UserContext = React.createContext();
 
-export const UserProvider = ({ children }) => (
-  <UserContext.Provider value={{ nickname }}>
-    {children}
-  </UserContext.Provider>
-);
+export const UserProvider = ({ children }) => {
+  // eslint-disable-next-line functional/no-let
+  let nickname = cookies.get('nickname');
+
+  if (isEmpty(nickname)) {
+    nickname = faker.name.findName();
+    cookies.set('nickname', nickname);
+  }
+
+  return (
+    <UserContext.Provider value={{ nickname }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContext;
---
+++ src/init.jsx
@@ -0,0 +1,65 @@
+import React, { createContext } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { unwrapResult } from '@reduxjs/toolkit';
+import io from 'socket.io-client';
+import gon from 'gon';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import App from './components/App';
+import { UserProvider } from './components/UserContext';
+import initStore from './redux/initStore';
+import channelsSlice, { fetchChannelsAsync } from './redux/slices/channels';
+import messagesSlice, { fetchMessagesByChannelIdsAsync } from './redux/slices/messages';
+import '../assets/application.scss';
+
+export const UserContext = createContext();
+
+const init = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    localStorage.debug = 'chat:*';
+  }
+
+  const preloadedState = {
+    channels: { channels: gon.channels, currentChannelId: gon.currentChannelId },
+    messages: { messages: gon.messages },
+  };
+
+  const store = initStore(preloadedState);
+  const { dispatch } = store;
+  const { actions: { addMessage } } = messagesSlice;
+  const {
+    actions: {
+      addChannel,
+      renameChannel,
+      removeChannel,
+    },
+  } = channelsSlice;
+
+  const socket = io();
+
+  const fetchChannelsAndMessages = () => dispatch(fetchChannelsAsync())
+    .then(unwrapResult)
+    .then((channels) => {
+      const channelIds = channels.map(({ id }) => id);
+      dispatch(fetchMessagesByChannelIdsAsync(channelIds));
+    });
+
+  socket.on('reconnect', fetchChannelsAndMessages);
+  socket.on('newChannel', ({ data: { attributes } }) => dispatch(addChannel(attributes)));
+  socket.on('renameChannel', ({ data: { attributes } }) => dispatch(renameChannel(attributes)));
+  socket.on('removeChannel', ({ data: { id } }) => dispatch(removeChannel(id)));
+  socket.on('newMessage', ({ data: { attributes } }) => dispatch(addMessage(attributes)));
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </Provider>,
+    document.getElementById('chat'),
+  );
+};
+
+export default init;
